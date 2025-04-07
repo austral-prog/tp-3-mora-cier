0b4b994ba6cd7636bd085b92d58f881d982d2fdd
mora-cier <mcier@mail.austral.edu.ar>
2025-04-07 15:33:29 -0300

    Mora.in_string.py

--- in_string.py
+++ in_string.py
@@ -3,16 +3,16 @@ def check_vowels():
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
 
-nombre = input("Inserte tu nombre: ")
+    nombre = input("Inserte tu nombre: ")
 
-nombre_low = nombre.lower()
+    nombre_low = nombre.lower()
 
-print("Contiene a:" , "a" in nombre_low)
+    print("Contiene a:" , "a" in nombre_low)
 
-print("Contiene e:" , "e" in nombre_low)
+    print("Contiene e:" , "e" in nombre_low)
 
-print("Contiene i:" , "i" in nombre_low)
+    print("Contiene i:" , "i" in nombre_low)
 
-print("Contiene o:" , "o" in nombre_low)
+    print("Contiene o:" , "o" in nombre_low)
 
-print("Contiene u:" , "u" in nombre_low)
+    print("Contiene u:" , "u" in nombre_low)
