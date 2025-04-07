fbfdfc76e45b487c5c75ed20dd54965bb10da310
mora-cier <mcier@mail.austral.edu.ar>
2025-04-07 15:59:32 -0300

    Mora.slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -4,6 +4,13 @@ def slice_simple():
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
     # Para verificar este ejercicio ejecutar el comando
     # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
-    
-    txt = input ("Escribe un texto o un numero de mas de 3 decimales: ")
-    print(txt[4: :2])
+
+    x = "Awesome"
+    x_real = x.lower()
+
+    print(x_real[ :3])    #Imprime las 3 primera letras del texto
+
+    mitad = int(len(x_real)/2)      #me dice cual es la mitad del texto // El int me quita la parte fraccional, no redondea
+    print(x_real[mitad - 1 : mitad + 2])      #DESDE mitad-2 HASTA mitad+1
+
+    print(x_real[ :4] + x[-3: ])
