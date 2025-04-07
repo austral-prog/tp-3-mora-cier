279bf0af08be17e5cfe823de733a961c06e13222
mora-cier <mcier@mail.austral.edu.ar>
2025-04-07 15:54:56 -0300

    Mora.tp3_slice_advanced_test.py

--- tp3_slice_advanced_test.py
+++ tp3_slice_advanced_test.py
@@ -18,3 +18,13 @@ class TP3SliceAdvancedTestCases(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+    x = "Awesome"
+    x_real = x.lower()
+
+    print(x_real[ :3])    #Imprime las 3 primera letras del texto
+
+    mitad = int(len(x_real)/2)      #me dice cual es la mitad del texto // El int me quita la parte fraccional, no redondea
+    print(x_real[mitad - 1 : mitad + 2])      #DESDE mitad-2 HASTA mitad+1
+
+    print(x_real[ :4] + x[-3: ])
