6d924acb7fddc15cb38cbba615c9347ade6639a5
mora-cier <mcier@mail.austral.edu.ar>
2025-04-07 15:53:29 -0300

    Mora.slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,7 +2,8 @@ def slice_simple():
     texto = "Awesome"
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    # Para verificar este ejercicio ejecutar el comando
+    # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    
+    txt = input ("Escribe un texto o un numero de mas de 3 decimales: ")
+    print(txt[4: :2])
