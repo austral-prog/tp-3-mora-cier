5fec71c4fb3412bf117137aed2b08ab448cc134d
mora-cier <mcier@mail.austral.edu.ar>
2025-04-07 15:45:30 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -6,9 +6,9 @@ def slice_advanced():
     x = "Awesome"
     x_real = x.lower()
 
-    print(x_real [ :3])    #Imprime las 3 primera letras del texto
+    print(x_real[ :3])    #Imprime las 3 primera letras del texto
 
     mitad = int(len(x_real)/2)      #me dice cual es la mitad del texto // El int me quita la parte fraccional, no redondea
-    print(x_real [mitad - 1 : mitad + 2])      #DESDE mitad-2 HASTA mitad+1
+    print(x_real[mitad - 1 : mitad + 2])      #DESDE mitad-2 HASTA mitad+1
 
     print(x_real[ :4] + x[-3: ])
