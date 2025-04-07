241eb1fdaaed80236a0c40599717abd23ebfa634
mora-cier <mcier@mail.austral.edu.ar>
2025-04-07 15:35:26 -0300

    Mora.slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,5 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+# Código a implementar utilizando input.
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
     
